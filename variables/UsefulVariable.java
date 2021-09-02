23e613a6a34fd6634421b37aee53a5cadb2b2c1c
yongjunyoo <yongjunyoo.dev@gmail.com>
2021-09-02 19:53:11 +0900

    2021.9.2 variables

---
+++ variables/UsefulVariable.java
@@ -0,0 +1,14 @@
+package org.opentutorials.javatutorials.variables;
+
+public class UsefulVariable {
+
+	public static void main(String[] args) {
+		int a = 100;
+		System.out.println(a+10);
+		System.out.println((a+10)/10);
+		System.out.println(((a+10)/10)-10);
+		System.out.println(((a+10)/10-10)*10);
+
+	}
+
+}
