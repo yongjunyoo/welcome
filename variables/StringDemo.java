bd01d25298a0bea15b0b52bf792872f8321ddb46
yongjunyoo <yongjunyoo.dev@gmail.com>
2021-09-02 19:49:06 +0900

    2021.9.2 variables

---
+++ variables/StringDemo.java
@@ -0,0 +1,15 @@
+package org.opentutorials.javatutorials.variables;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String a, b;
+		a = "coding";
+		b = "everybody";
+		System.out.println(a+b);
+		
+		
+
+	}
+
+}
